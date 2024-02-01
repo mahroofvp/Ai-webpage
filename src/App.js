0fe5af94cca3f95a6abbb602d5edad20d1daddf3
Mahroof VP <mahroofvp77@gmail.com>
2024-02-01 18:22:43 +0530

    completed Ai website

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css'
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import {Article, Brand, CTA, Feature, Navbar} from './Component'
+
+
+function App() {
+
+ 
+  return (
+    
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+  
+
+export default App;
